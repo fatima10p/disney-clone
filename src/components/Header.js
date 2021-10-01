d0ee5a076dc20d9239a1879cc2d99941773d6a0e
Fatima Zahid <fatima.zahid@10pearls.com>
2021-10-01 20:51:53 +0500

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <Nav>
+            <Logo src="/images/logo.svg" />
+            <NavMenu>
+                <a href="/home">
+                    <img src="/images/home-icon.svg" alt="home" />
+                    <span>HOME</span>
+                </a>
+                <a href="/search">
+                    <img src="/images/search-icon.svg" alt="search" />
+                    <span>SEARCH</span>
+                </a>
+                <a href="/watchlist">
+                    <img src="/images/watchlist-icon.svg" alt="watchlist" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a href="/original">
+                    <img src="/images/original-icon.svg" alt="original" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a href="/movies">
+                    <img src="/images/movie-icon.svg" alt="movie" />
+                    <span>MOVIES</span>
+                </a>
+                <a href="/series">
+                    <img src="/images/series-icon.svg" alt="series" />
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+                <UserImg src="/images/profile.png" alt="profile" />
+        </Nav>
+    )
+}
+
+export default Header
+
+const Nav = styled.div`
+    height: 70px;
+    background: black;
+    display: flex; 
+    align-items: center;
+    padding: 0 36px; 
+`
+
+const Logo = styled.img`
+    width: 80px;
+`
+
+const NavMenu = styled.div`
+    display: flex;
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
+
+    a {
+        display: flex;
+        align.-items: center;
+        padding: 0 12px;
+        cursor: pointer;
+        color: white;
+        text-decoration: none;
+        
+        img {
+            height: 20px;
+        }
+        span {
+            font-size: 15px;
+            letter-spacing: 1.42px;
+            position: relative;
+
+            &:after {
+                content: "";
+                height: 2px;
+                background: white;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: -6px;
+                opacity: 0;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform: scaleX(0);
+            }
+        }
+
+        &:hover {
+            span:after {
+                transform: scaleX(1);
+                opacity: 1;
+            }
+        }
+    }
+`
+
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
+
+`
